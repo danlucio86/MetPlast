e668d7ce7efa6e07b7bfb8ab226e28b27597f974
danlucio86 <48597177+danlucio86@users.noreply.github.com>
2021-11-03 14:37:15 +0100

    Add files via upload

--- R/MetPlast_int.R
+++ R/MetPlast_int.R
@@ -118,5 +118,5 @@ numb_peaks <- apply (Data_numb_peaks, MARGIN = 2, FUN = sum)}
 my_filter_fc <- function(Dj_index_weight) {
   Dj_index_weight %>%
     dplyr::group_by(Sample) %>%
-    dplyr::filter(Pij_Si == .data[[max (Pij_Si)]])
+    dplyr::filter(Pij_Si == {{Pij_Si}})
 }
