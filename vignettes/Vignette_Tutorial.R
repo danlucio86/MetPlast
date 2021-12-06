b7bc1052bc8a473c2fb481bafe3cc94b313d4c56
danlucio86 <48597177+danlucio86@users.noreply.github.com>
2021-12-06 18:33:14 +0100

    Add files via upload

---
+++ vignettes/Vignette_Tutorial.R
@@ -0,0 +1,8 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+## -----------------------------------------------------------------------------
+# devtools::install_github ("danlucio86/MetPlast")
+
+library("MetPlast")
+
